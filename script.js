4335d8e80c1f43f272d7638f6f724314353ee48f
samillefatima <120414373+samillefatima@users.noreply.github.com>
2024-09-23 00:30:43 -0300

    att

--- script.js
+++ script.js
@@ -55,3 +55,54 @@ document.getElementById('saiba_mais_btn').addEventListener('click', function() {
         this.innerText = 'Saiba mais';
     }
 });
+document.getElementById('saiba_mais_btn2').addEventListener('click', function() {
+    var moreInfo = document.getElementById('more-infoi');
+    if (moreInfo.classList.contains('hidden')) {
+        moreInfo.classList.remove('hidden');
+        moreInfo.classList.add('show');
+        this.innerText = 'Mostrar menos';
+    } else {
+        moreInfo.classList.add('hidden');
+        moreInfo.classList.remove('show');
+        this.innerText = 'Saiba mais';
+    }
+});
+document.getElementById('saiba_mais_btn3').addEventListener('click', function() {
+    var moreInfo = document.getElementById('more-infof');
+    if (moreInfo.classList.contains('hidden')) {
+        moreInfo.classList.remove('hidden');
+        moreInfo.classList.add('show');
+        this.innerText = 'Mostrar menos';
+    } else {
+        moreInfo.classList.add('hidden');
+        moreInfo.classList.remove('show');
+        this.innerText = 'Saiba mais';
+    }
+});
+
+function calcularIMC() {
+    const peso = parseFloat(document.getElementById('peso').value);
+    const altura = parseFloat(document.getElementById('altura').value);
+    const resultadoDiv = document.getElementById('resultado');
+
+    if (isNaN(peso) || isNaN(altura) || peso <= 0 || altura <= 0) {
+        resultadoDiv.innerHTML = "Por favor, insira valores válidos para peso e altura.";
+        return;
+    }
+
+    const imc = peso / (altura * altura);
+
+
+
+    if (imc < 18.5) {
+        categoria = 'Abaixo do peso';
+    } else if (imc >= 18.5 && imc < 24.9) {
+        categoria = 'Peso normal';
+    } else if (imc >= 25 && imc < 29.9) {
+        categoria = 'Sobrepeso';
+    } else {
+        categoria = 'Obesidade';
+    }
+
+    resultadoDiv.innerHTML = `Seu IMC é ${imc.toFixed(2)} (${categoria})`;
+}
